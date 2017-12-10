1f6b32f8226a614f41ea61e50931dcb4c1a70eb2
spy <sp831031@gmail.com>
2017-12-10 17:17:49 +0800

    add show page for public user

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,15 @@
 class RestaurantsController < ApplicationController
-  
+  before_action :set_restaurant, only: [:show]
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
+  end
+
+  def show
+    
+  end
+
+  private
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])  
   end
 end
