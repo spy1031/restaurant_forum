48df234428d0364fc1b9ddcc1cd8724ddc7a30f0
spy <sp831031@gmail.com>
2018-01-07 13:24:33 +0800

    user can update the profile's name and intro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show,:edit,:update]
   def show
-    @user = User.find(params[:id])    
+    
+  end
+
+  def edit
+    
+    if @user != current_user
+      redirect_to user_path(@user)
+    end
+    
+  end
+
+  def update
+   
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+      flash[:notice] = "profile was successfully updated"
+    else
+      render :edit
+      flash[:alert] = "profile was failed to update"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:avatar, :intro)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
