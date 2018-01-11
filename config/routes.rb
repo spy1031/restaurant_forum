5025f50c1b16f6eb1b919769247d1344c319dd1f
spy <sp831031@gmail.com>
2018-01-11 13:03:47 +0800

    setup routes for feeds and dashboard pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
   
   resources :restaurants, only:[:index ,:show] do 
     resources :comments, only:[:create,:destroy]
-    
+    # 瀏覽所有餐廳的最新動態
+    collection do
+      get :feeds
+    end
+
+    # 瀏覽個別餐廳的 Dashboard
+    member do
+      get :dashboard
+    end
   end
   root "restaurants#index"
   resources :users, only:[:show,:edit,:update]
