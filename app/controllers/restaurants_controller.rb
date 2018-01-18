edbc9722b47466a9bd9d8d22cf65e54c90275079
spy <sp831031@gmail.com>
2018-01-18 12:37:27 +0800

    user can link to dashboard from restaurant show page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show]
+  before_action :set_restaurant, only: [:show, :dashboard]
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
     @categories = Category.all
@@ -14,6 +14,10 @@ class RestaurantsController < ApplicationController
   def feeds
     @recent_restaurants = Restaurant.order(created_at: :desc).limit(10)
     @recent_comments = Comment.order(created_at: :desc).limit(10)
+  end
+
+  def dashboard
+    
   end
 
   private
