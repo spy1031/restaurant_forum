aeb8d71850e80618dd0fbcf529185d597e0e20b7
spy <sp831031@gmail.com>
2018-01-19 14:04:51 +0800

    create favorite model and setup relation among user,restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,6 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   belongs_to :category, optional: true
   has_many :comments ,dependent: :destroy
+  has_many :favorites , dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :comments ,dependent: :restrict_with_error
   has_many :restaurants, through: :comments
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
   mount_uploader :avatar, AvatarUploader
   def admin?
     self.role == "admin"
