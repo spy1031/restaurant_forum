948b85877f89f74ee6091480843a7508011f4fde
spy <sp831031@gmail.com>
2018-01-27 20:20:18 +0800

    add un/like action on RestaurantController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite]
+  before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite, :like, :unlike]
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
     @categories = Category.all
@@ -30,6 +30,18 @@ class RestaurantsController < ApplicationController
     favorite.destroy
     redirect_back(fallback_location: root_path)
   end
+
+  def like
+    @restaurant.likes.create!(user: current_user)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def unlike
+    like = Like.where(restaurant: @restaurant, user: current_user).first
+    like.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
   private
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])  
