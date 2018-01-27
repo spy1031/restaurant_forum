b94b8eb4024b51197174f91da43b145e9f173e12
spy <sp831031@gmail.com>
2018-01-27 19:56:54 +0800

    setup routes for un/like

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
       get :dashboard # 瀏覽個別餐廳的 Dashboard
       post :favorite
       post :unfavorite
+      post :like
+      post :unlike
     end
   end
   root "restaurants#index"
