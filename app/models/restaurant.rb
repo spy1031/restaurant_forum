ad6142f41ba138fbe4998c9af888e56eb90d5c7e
spy <sp831031@gmail.com>
2018-01-27 13:52:43 +0800

    setup relation among user,restaurant and like

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,8 +4,9 @@ class Restaurant < ApplicationRecord
   belongs_to :category, optional: true
   has_many :comments ,dependent: :destroy
   has_many :favorites , dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
-
+  has_many :liked_users, through: :likes, source: :user
   def is_favorited?(user)
     self.favorited_users.include?(user)
   end
