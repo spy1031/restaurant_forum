819ce0af47f030ad3911f2c5b27e377e069c2963
spy <sp831031@gmail.com>
2018-01-28 11:25:06 +0800

    add is_liked? method to restaurant model for checking un/like button

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,4 +10,7 @@ class Restaurant < ApplicationRecord
   def is_favorited?(user)
     self.favorited_users.include?(user)
   end
+  def is_liked?(user)
+    self.liked_users.include?(user)
+  end
 end
