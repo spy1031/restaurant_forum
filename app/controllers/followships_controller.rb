2753ace0a9655ac597f7aa9b8431b760b0f5d7ea
spy <sp831031@gmail.com>
2018-01-30 21:14:41 +0800

    validate uniqueness of following_id in user_id scope and add create action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,2 +1,12 @@
 class FollowshipsController < ApplicationController
+
+  def create
+    @followships = current_user.followships.build(following_id: params[:following_id])
+    if @followships.save
+      flash[:notice] = "Successfully followed"
+    else
+      flash[:alert] = @followships.errors.full_messages.to_sentence      
+    end
+    redirect_back(fallback_location: root_path)
+  end
 end
