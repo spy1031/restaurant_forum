da172fa53d041844c3b772e1bcc515e206c926d2
spy <sp831031@gmail.com>
2018-02-01 14:15:50 +0800

    setup self-referential relationships within users table through friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,19 @@ class User < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :likes, dependent: :destroy
+  
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
   has_many :liked_restaurants, through: :likes, source: :restaurant
+  
   has_many :followships, dependent: :destroy
   has_many :followings, through: :followships
-
-
-  has_many :inverse_followships ,class_name: "Followship",
-  foreign_key: "following_id"
+  has_many :inverse_followships ,class_name: "Followship", foreign_key: "following_id"
   has_many :followers, through: :inverse_followships, source: :user
+
+  has_many :friendships, dependent :destroy
+  has_many :friends, through: :friendships
+  
+  
 
   mount_uploader :avatar, AvatarUploader
   def admin?
